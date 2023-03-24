494efbe664249c03b01d70d27f8febf69493ef09
chetan <chetankumar70174577@gmail.com>
2023-03-24 19:56:11 +0530

    Nested member functions

--- Simple_Program_of_class.cpp
+++ Simple_Program_of_class.cpp
@@ -3,27 +3,31 @@
 using namespace std;
 class student
 {
-    int roll_no;
-    char name[10];
+    char name[30];
+    int rollNo;
     public:
-    void getdata();
-    void display();
+    void getdata(char n[30],int r);
+    void print()
+    {
+        cout << "Name : " <<name<< endl;
+        cout << "Roll no : " <<rollNo<< endl;
+    }
 };
- void student:: getdata()
-    {
-        cout << "Enter the roll no : ";
-        cin >> roll_no;
-        cout << "Enter the name : ";
-        cin >> name;
-    }
-    void student:: display()
-    {
-        cout << "Name is : " << name << endl;
-        cout << "Roll No is : " << roll_no;
-    }
+void student :: getdata(char n[30],int r)
+{
+        strcpy(name,n);
+        rollNo = r;
+}
 int main()
 {
-    student s;
-    s.getdata();
-    s.display();
+    char ch[30] = "chetan";
+    char ch1[30] = "gourav";
+    student s1,s2;
+    cout << ch<< "'s details :" << endl;
+    s1.getdata(ch,20);
+    s1.print();
+    cout <<  ch1<< "'s details :" << endl;
+    s2.getdata(ch1,10);
+    s2.print();
+    return 0;
 }
---
+++ tempCodeRunnerFile.cpp
@@ -0,0 +1,3 @@
+cout << "Student 2 details :" << endl;
+    // s2.getdata(ch1,10);
+    // s2.print();
