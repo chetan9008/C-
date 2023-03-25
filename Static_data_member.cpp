88c0ab2d436b03817e27ca6a990c4e662fbb4c1b
chetan <chetankumar70174577@gmail.com>
2023-03-25 18:29:18 +0530

    Static data member

---
+++ Static_data_member.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+class Staticdata
+{
+    static int count;
+    public:
+    void increment()
+    {   
+        count++;
+    }
+    void display()
+    {
+        cout << "Value of count is : " << count<< endl;
+    }
+};
+int Staticdata :: count;
+int main()
+{
+    Staticdata a,b,c;
+    cout << "count value for all objects:" << endl;
+    a.display();
+    b.display();
+    c.display();
+    a.increment();
+    b.increment();
+    c.increment();
+    cout << "After incrementing :"<<endl;
+    a.display();
+    b.display();
+    c.display();
+}
