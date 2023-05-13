672d4cd8e3bec7267698a9a8df136eb495c0f070
chetan <chetankumar70174577@gmail.com>
2023-05-13 23:25:10 +0530

    Complent in c++

--- Programs/1sComplement_with_nested_member_function.cpp
+++ Programs/1sComplement_with_nested_member_function.cpp
@@ -1,9 +1,11 @@
 #include<iostream>
+#include<windows.h>
 using namespace std;
 int main()
 {
+    system("cls");
     int *ptr;
     int a = 29;
     ptr[0] = a;
-    cout << ptr[0] << endl;
+    cout << "Value by pointer is " << ptr[0] << endl;
 }
