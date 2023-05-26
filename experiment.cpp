a104dba5d4f7682cf51bbf1b2453aa88c7a8f836
chetan <chetankumar70174577@gmail.com>
2023-05-26 15:02:50 +0530

    experimenting in c++

--- experiment.cpp
+++ experiment.cpp
@@ -1,16 +1,33 @@
-#include<iostream>
-#include<fstream>
+#include <iostream>
+#include <fstream>
+int const size = 300;
 using namespace std;
 int main()
 {
-    ofstream outf("randi.html");
-    char script[200];
-    cout << "Enter the code";
-    gets(script);
-    outf << script << "\n";
-    outf.close();
-    ifstream input("randi.html");
-    input >> script ;
-    cout << script << endl;
+    char code[size];
+    ofstream output;
+    output.open("country.html");
+    output << "<p>Usa</p>\n<p>India</p>\n";
+    output.close();
+    output.open("capital.html");
+    output << "<p>Washinton DC</p>\n<p>New delhi</p>";
+    output.close();
+    ifstream inp1, inp2;
+    inp1.open("capital.html");
+    inp2.open("country.html");
+    while (1)
+    {
+            if(inp1.eof())
+            exit(0);
+            inp1.getline(code, size);
+            cout << code << endl;
+            if(inp2.eof())
+            exit(0);
+            inp2.getline(code, size);
+            cout << code << endl;
+    }
+
+    inp1.close();
+    inp2.close();
     return 0;
 }
