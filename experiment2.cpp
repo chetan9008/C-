8c3f003192716c7ef332bd50b29537bdb751445d
chetan <chetankumar70174577@gmail.com>
2023-08-03 11:59:05 +0530

    Tik tak toe project

--- experiment2.cpp
+++ experiment2.cpp
@@ -18,5 +18,26 @@ int main()
     void(*fptr)(int,int) = add; //this is same as below
     // auto fptr = add;
     fptr(2,3);
+
+    int ch[2][3];
+
+    for(auto i=0;i<2;i++)
+    {
+        for(auto j=0;j<3;j++)
+        {
+            cout << "Enter :";
+            cin >> ch[i][j];
+        }
+        cout << endl;
+    }
+
+    for (auto i = 0; i < 2; i++)
+    {
+        for (auto j = 0; j < 3; j++)
+        {
+            cout << ch[i][j];
+        }
+        cout << endl;
+    }
     return 0;
 }
