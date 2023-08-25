86639019279b61e5e113d019c7c4d581d0cc78ca
chetan <chetankumar70174577@gmail.com>
2023-08-25 16:40:27 +0530

    constructors and destructors

--- experiment.cpp
+++ experiment.cpp
@@ -1,35 +1,7 @@
 #include<bits/stdc++.h>
 using namespace std;
-class s
-{
-    static int counter;
-    int data;
-    public:
-    s(int x)
-    {
-        data = x;
-        counter++;
-    }
-    void display();
-    static void c_value()
-    {
-        cout << "Counter is " << counter << endl;
-    }
-};
-int s::counter;
-void s::display()
-{
-    cout << "Data is " << data << endl;
-}
+
 int main()
 {
-    s var(10);
-    var.display();
-    s var1(20);
-    var1.display();
-    s var2(30);
-    var2.display();
-    var.display();
-    s::c_value();
     return 0;
 }
