0f122abfdc63b8ffe052e275f519ee5e7c543d5d
chetan <chetankumar70174577@gmail.com>
2023-08-27 15:37:51 +0530

    practising inheritance in c++

--- experiment.cpp
+++ experiment.cpp
@@ -1,7 +1,44 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
+class base
+{
+protected:
+    int b;
 
+public:
+    base(int x)
+    {
+        b = x;
+        cout << "Base class "  << endl;
+    }
+};
+class derived1:public base 
+{
+    protected:
+    int d;
+    public:
+    derived1(int x,int y):base(x),d(y)
+    {
+        cout << "Derived 1 class" << endl;
+    }
+};
+class derived2:public derived1
+{
+    public:
+    derived2(int x,int y):derived1(x,y)
+    {
+        cout << "Derived 2 class" << endl;
+    }
+    void display();
+};
+void derived2::display()
+{
+    cout << "Base class value is " << b << endl;
+    cout << "Derived class value is " << d << endl;
+}
 int main()
 {
+    derived2 d(2,3);
+    d.display();
     return 0;
 }
--- tempCodeRunnerFile.cpp
+++
@@ -1,6 +0,0 @@
-if ((row != 1 || col != 1 || choice != 1) && (row != 2 || col != 2 || choice != 2) && (row != 3 || col != 3 || choice != 3) && (row != 4 || col != 4 || choice != 4) && (row != 5 || col != 5 || choice != 5) && (row != 6 || col != 6 || choice != 6) && (row != 7 || col != 7 || choice != 7) && (row != 8 || col != 8 || choice != 8) && (row != 9 || col != 9 || choice != 9))
-// {
-//     // gotoxy(50, 35);
-//     cout << "Please enter valid row";
-//     // enter_value();
-// }
