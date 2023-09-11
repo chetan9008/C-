f8675b37185f9c021419cfb9e9e6bb7cb2133caa
chetan <chetankumar70174577@gmail.com>
2023-09-11 12:00:29 +0530

    practising for exam

--- experiment.cpp
+++ experiment.cpp
@@ -1,6 +1,29 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
+class base
+{
+    protected:
+    double x,y;
+    public:
+    void get(double xx,double yy)
+    {
+        x =xx ;
+        y = yy;
+    }
+    virtual void display() =0;
+};
+class traingle : public base
+{
+    public:
+    void display()
+    {
+        cout << "Area is " << ((1.0/2.0) * x * y);
+    }
+};
 int main()
 {
+    traingle *t = new traingle;
+    t->get(2.2,3.3);
+    t->display();
     return 0;
 }
